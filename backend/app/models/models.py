8c67c061d384f41123976ab98e474b803c20bba6
ymakwan1 <ymakwan1@binghamton.edu>
2025-08-06 11:52:13 -0400

    SQLAlchemy Song model for normalized song data

---
+++ backend/app/models/models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    index = db.Column(db.Integer, autoincrement=True)
+    id = db.Column(db.String(255), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    danceability = db.Column(db.Float, nullable=False)
+    energy = db.Column(db.Float, nullable=False)
+    key = db.Column(db.Integer, nullable=False)
+    loudness = db.Column(db.Float, nullable=False)
+    mode = db.Column(db.Integer, nullable=False)
+    accousticness = db.Column(db.Float, nullable=False)
+    instrumentalness = db.Column(db.Float, nullable=False)
+    liveness = db.Column(db.Float, nullable=False)
+    valence = db.Column(db.Float, nullable=False)
+    tempo = db.Column(db.Float, nullable=False)
+    duration_ms = db.Column(db.Integer, nullable=False)
+    time_signature = db.Column(db.Integer, nullable=False)
+    num_bars = db.Column(db.Integer, nullable=False)
+    num_sections = db.Column(db.Integer, nullable=False)
+    nums_segments = db.Column(db.Integer, nullable=False)
+    class_ = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False, default=0.0)
+
+    def __repr__(self):
+        return f"<Song {self.title} - ID: {self.id}>"
+
