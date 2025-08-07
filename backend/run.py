0aad6ad728cc63b4f8b16f9667d71f744d83b1a1
ymakwan1 <ymakwan1@binghamton.edu>
2025-08-07 14:16:12 -0400

    docker config updated

--- backend/run.py
+++ backend/run.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         except Exception as e:
             app.logger.error(f"Error normalizing and inserting song data: {e}")
             
-    app.run()
+    app.run(host="0.0.0.0", port=5001)
